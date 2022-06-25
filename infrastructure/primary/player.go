715105ef81ff89e7f293634bcc2e9f6cff1d76f9
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2022-06-25 20:10:40 +0200

    refactor(infra): extract parsing logic in its own function

--- infrastructure/primary/player.go
+++ infrastructure/primary/player.go
@@ -6,15 +6,21 @@ import (
 )
 
 func main() {
-	var N int
-	fmt.Scan(&N)
-
-	telephoneNumbers := make([]string, N)
-	for i := 0; i < N; i++ {
-		fmt.Scan(&telephoneNumbers[i])
-	}
+	telephoneNumbers := parseInputs()
 
 	minimumNumberCount := usecases.FindMinimumNumberCountUsecase(telephoneNumbers)
 
 	fmt.Println(minimumNumberCount)
+}
+
+func parseInputs() []string {
+	var telephoneNumberCount int
+	fmt.Scan(&telephoneNumberCount)
+
+	telephoneNumbers := make([]string, telephoneNumberCount)
+	for i := 0; i < telephoneNumberCount; i++ {
+		fmt.Scan(&telephoneNumbers[i])
+	}
+
+	return telephoneNumbers
 }
