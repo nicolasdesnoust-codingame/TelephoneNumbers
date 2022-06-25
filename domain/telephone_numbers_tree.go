edab45b550e59297200d64d49f696174f77cfa50
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2022-06-26 00:09:07 +0200

    refactor(domain): simplify registration of a telephone number

--- domain/telephone_numbers_tree.go
+++ domain/telephone_numbers_tree.go
@@ -11,16 +11,6 @@ func NewTelephoneNumbersTree() *TelephoneNumbersTree {
 }
 
 func (tree *TelephoneNumbersTree) RegisterTelephoneNumber(telephoneNumber *TelephoneNumber) {
-	insertionPoint := tree.findInsertionPoint(telephoneNumber)
-	tree.insertRemainingNumbers(insertionPoint, telephoneNumber)
-}
-
-type insertionPoint struct {
-	numberIndex int
-	node        *TreeNode
-}
-
-func (tree *TelephoneNumbersTree) findInsertionPoint(telephoneNumber *TelephoneNumber) *insertionPoint {
 	currentNode := tree.rootNode
 
 	numberIndex := 0
@@ -28,26 +18,11 @@ func (tree *TelephoneNumbersTree) findInsertionPoint(telephoneNumber *TelephoneN
 		number := telephoneNumber.GetNumberByIndex(numberIndex)
 
 		if !currentNode.hasChildWithValue(number) {
-			break
+			currentNode.addChildWithValue(number)
+			tree.size++
 		}
 
-		childNode := currentNode.getChildByValue(number)
-		currentNode = childNode
-	}
-
-	return &insertionPoint{numberIndex: numberIndex, node: currentNode}
-}
-
-func (tree *TelephoneNumbersTree) insertRemainingNumbers(insertionPoint *insertionPoint, telephoneNumber *TelephoneNumber) {
-	previousNode := insertionPoint.node
-	var currentNode *TreeNode
-
-	for i := insertionPoint.numberIndex; i < telephoneNumber.Size(); i++ {
-		currentValue := telephoneNumber.GetNumberByIndex(i)
-		currentNode = previousNode.addChildWithValue(currentValue)
-		tree.size++
-
-		previousNode = currentNode
+		currentNode = currentNode.getChildByValue(number)
 	}
 }
 
